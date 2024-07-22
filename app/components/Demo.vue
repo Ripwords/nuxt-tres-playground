47716763742674ae9950a5229a535a80f774b4f3
JJ Teoh <teohjjteoh@gmail.com>
2024-07-22 21:35:37 +0800

    first commit

---
+++ app/components/Demo.vue
@@ -0,0 +1,8 @@
+<template>
+  <TresCanvas window-size clear-color="#82dbc5">
+    <TresMesh>
+      <TresBoxGeometry :args="[2, 1, 1]" />
+      <TresMeshBasicMaterial color="yellow" />
+    </TresMesh>
+  </TresCanvas>
+</template>
